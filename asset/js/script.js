6b7e1097eebfa27efa496250dd0fa9511a545345
Azzam Maulana Dzakwan <azzamdzakwan088@gmail.com>
2025-03-04 13:31:42 +0700

    commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,5 +1,3 @@
-
-
 // Dropdown
 document.addEventListener("click", (e) => {
   const isDropdownButton = e.target.matches("[data-dropdown-button]");
@@ -16,42 +14,4 @@ document.addEventListener("click", (e) => {
   });
 });
 
-// Slider Mobile Project
-let items = document.querySelectorAll(".slider .list .item");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let thumbnails = document.querySelectorAll(".thumbnail .item");
 
-// Config Params
-let countItem = items.length;
-let itemActive = 0;
-
-// Event Next Click
-next.onclick = function () {
-  itemActive = itemActive + 1;
-  if (itemActive >= countItem) {
-    itemActive = 0;
-  }
-  showSlider();
-};
-
-// Event Previous Click
-prev.onclick = function () {
-  itemActive = itemActive - 1;
-  if (itemActive < 0) {
-    itemActive = countItem - 1;
-  }
-  showSlider();
-};
-
-function showSlider() {
-  // Remove old active item
-  let itemActiveOld = document.querySelector(".slider .list .item.active");
-  let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
-  if (itemActiveOld) itemActiveOld.classList.remove("active");
-  if (thumbnailActiveOld) thumbnailActiveOld.classList.remove("active");
-
-  // Activate new item
-  items[itemActive].classList.add("active");
-  thumbnails[itemActive].classList.add("active");
-}
